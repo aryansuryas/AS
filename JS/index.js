de00f1e1b1bc1d661e7ab9f8c1dd879eb5884e23
Aryan Surya S <aryansuryasgowda@gmail.com>
2025-11-13 11:39:39 +0530

    Enhance JS notes with console logs and comments
    Added various console log statements and comments to demonstrate JavaScript basics.

--- JS/index.js
+++ JS/index.js
@@ -1,1 +1,40 @@
-console.lo
+console.log(`hello`);   // output basic 
+console.log(`this is my JS notes`);  // for output : inspect -> console
+
+// window.alert(`This is an aleeeert`);  // to give alert
+// this is a comment
+/* this is
+ a
+  multiline
+comments */ 
+
+
+ document.getElementById("a");
+         
+document.getElementById("b").textContent = `This is a JS notes`;
+ /* this is used to print the text on display here without (.textContent = `hello ` ;) it will print the text which is in html or else what u give in JS */
+
+
+ // 2
+ let y;  // declaration
+ y=12.00;  // Initialsisation
+ let x=21;      // Declaration +  Initialsisation
+console.log(y);
+console.log(x);
+let gpa = 12.44;
+console.log(`you are  ${x} years old  `);
+ /* To print the names use ` ` and for the value/ variable use ${} 
+dollar for value and inside curly braces mention the variable name.*/
+console.log(`you  have  ${gpa}  gpa`);
+console.log(typeof gpa); // shows the data type
+
+
+let name = `Aryan Surya S`;
+let food = `pizza`
+console.log(typeof name);
+console.log(` my name is  ${name} .`);
+console.log(typeof  food);
+console.log(` my Fav Food is  ${food}.`);
+
+
+le
