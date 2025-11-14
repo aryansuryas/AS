7e8738a77e90683245f06f70855401eacedac7ce
Aryan Surya S <aryansuryasgowda@gmail.com>
2025-11-14 11:34:59 +0530

    Refactor console logs and variable declarations

--- JS/index.js
+++ JS/index.js
@@ -1,40 +1,61 @@
-console.log(`hello`);   // output basic 
-console.log(`this is my JS notes`);  // for output : inspect -> console
+console.log(`hello`); // output basic
+console.log(`this is my JS notes`); // for output : inspect -> console
+
 
 // window.alert(`This is an aleeeert`);  // to give alert
 // this is a comment
 /* this is
  a
   multiline
-comments */ 
+comments */
 
 
- document.getElementById("a");
-         
+
+
+document.getElementById("a");
+
+
 document.getElementById("b").textContent = `This is a JS notes`;
- /* this is used to print the text on display here without (.textContent = `hello ` ;) it will print the text which is in html or else what u give in JS */
+/* this is used to print the text on display here without (.textContent = `hello ` ;) it will print the text which is in html or else what u give in JS */
 
 
- // 2
- let y;  // declaration
- y=12.00;  // Initialsisation
- let x=21;      // Declaration +  Initialsisation
+
+
+// 2
+let y; // declaration
+y = 12.00; // Initialsisation
+let x = 21; // Declaration +  Initialsisation
 console.log(y);
 console.log(x);
 let gpa = 12.44;
 console.log(`you are  ${x} years old  `);
- /* To print the names use ` ` and for the value/ variable use ${} 
+/* To print the names use ` ` and for the value/ variable use ${}
 dollar for value and inside curly braces mention the variable name.*/
 console.log(`you  have  ${gpa}  gpa`);
 console.log(typeof gpa); // shows the data type
+
+
 
 
 let name = `Aryan Surya S`;
 let food = `pizza`
 console.log(typeof name);
 console.log(` my name is  ${name} .`);
-console.log(typeof  food);
+console.log(typeof food);
 console.log(` my Fav Food is  ${food}.`);
 
 
-le
+let online = true; // boolean (TRUE : FALSE)
+console.log(typeof online);
+console.log(`bro is online : ${online}`);
+
+
+let fullname = `AS`;
+let age = 14;
+let std = false;
+
+
+document.getElementById("p1").textContent = ` YOUR NAME IS ${fullname} `; // PRINTING VARIABLES USING
+document.getElementById("p2").textContent = `YOUR AGE IS ${age}`; // FOR ID " " FOR VALUES ` `
+document.getElementById("p3").textContent = ` ARE U A STUDENT ? ? ${std }`;
+  body{
