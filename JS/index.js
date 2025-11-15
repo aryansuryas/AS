5536f086a60b6b2c94af2584452c54bdd6b322b2
Aryan Surya S <aryansuryasgowda@gmail.com>
2025-11-15 23:28:33 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,5 @@
 console.log(`hello`); // output basic
 console.log(`this is my JS notes`); // for output : inspect -> console
-
-
 // window.alert(`This is an aleeeert`);  // to give alert
 // this is a comment
 /* this is
@@ -9,17 +7,9 @@ console.log(`this is my JS notes`); // for output : inspect -> console
   multiline
 comments */
 
-
-
-
 document.getElementById("a");
-
-
 document.getElementById("b").textContent = `This is a JS notes`;
 /* this is used to print the text on display here without (.textContent = `hello ` ;) it will print the text which is in html or else what u give in JS */
-
-
-
 
 // 2
 let y; // declaration
@@ -35,8 +25,6 @@ console.log(`you  have  ${gpa}  gpa`);
 console.log(typeof gpa); // shows the data type
 
 
-
-
 let name = `Aryan Surya S`;
 let food = `pizza`
 console.log(typeof name);
@@ -44,11 +32,9 @@ console.log(` my name is  ${name} .`);
 console.log(typeof food);
 console.log(` my Fav Food is  ${food}.`);
 
-
 let online = true; // boolean (TRUE : FALSE)
 console.log(typeof online);
 console.log(`bro is online : ${online}`);
-
 
 let fullname = `AS`;
 let age = 14;
