1c92e245ddf2f668ce823ad75b2762ecaec22d8c
Aryan Surya S <aryansuryasgowda@gmail.com>
2025-11-19 09:43:10 +0530

    Update index.js

--- JS/index.js
+++ JS/index.js
@@ -1,17 +1,56 @@
+console.log("HELLO ");  // to print u can use " " or ` ` 
+console.log(`Hiiiiiiiiiii   `); 
+console.log(" THIS IS MY FIRST JS FILE");
+window.alert("HI FROM AS");  // to give alert popup on screen
+// single line comment
+/* Multi Line Comment */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+
 console.log(`hello`); // output basic
 console.log(`this is my JS notes`); // for output : inspect -> console
 // window.alert(`This is an aleeeert`);  // to give alert
 // this is a comment
-/* this is
+this is
  a
-  multiline
-comments */
+multiline
+comments 
 
-document.getElementById("a");
-document.getElementById("b").textContent = `This is a JS notes`;
-/* this is used to print the text on display here without (.textContent = `hello ` ;) it will print the text which is in html or else what u give in JS */
+document.getElementById("a")  ;
+console.log(a);
 
-// 2
+ this is used to print the text on display here without (.textContent = `hello ` ;) it will print the text which is in html or else what u give in JS 
+
+
+//*
 let y; // declaration
 y = 12.00; // Initialsisation
 let x = 21; // Declaration +  Initialsisation
@@ -19,8 +58,8 @@ console.log(y);
 console.log(x);
 let gpa = 12.44;
 console.log(`you are  ${x} years old  `);
-/* To print the names use ` ` and for the value/ variable use ${}
-dollar for value and inside curly braces mention the variable name.*/
+ To print the names use ` ` and for the value/ variable use ${}
+dollar for value and inside curly braces mention the variable name.  
 console.log(`you  have  ${gpa}  gpa`);
 console.log(typeof gpa); // shows the data type
 
@@ -39,9 +78,13 @@ console.log(`bro is online : ${online}`);
 let fullname = `AS`;
 let age = 14;
 let std = false;
+*/
 
-let char = "apple"
-document.getElementById("p1").textContent = ` YOUR NAME IS ${fullname} `; // PRINTING VARIABLES USING
-document.getElementById("p2").textContent = `YOUR AGE IS ${age}`; // FOR ID " " FOR VALUES ` `
-document.getElementById("p3").textContent = ` ARE U A STUDENT ? ? ${std }`;
-  body{
+ // document.getElementById("p1").textContent = ` YOUR NAME IS ${fullname} ` ; // PRINTING VARIABLES USING
+ // document.getElementById("p2").textContent = `YOUR AGE IS ${age}`; // FOR ID " " FOR VALUES ` `
+ // document.getElementById("p3").textContent = ` ARE U A STUDENT ? ? ${std }`;
+  
+// document.getElementById("b").textContent = `This is a JS notes`;
+ // let char = "apple" ;
+
+ 
