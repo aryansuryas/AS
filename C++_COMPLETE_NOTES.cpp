0e503775be0fc35696b60729eb88ed43b1f19c98
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-01-06 23:51:16 +0530

    Update C++_COMPLETE_NOTES.cpp

--- C++_COMPLETE_NOTES.cpp
+++ C++_COMPLETE_NOTES.cpp
@@ -13,7 +13,7 @@ int main(){
     return 0;
 }
 
-//
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
  #include <iostream>
  int main(){
     std::cout <<"Hello World AS" << std::endl; // << std::endl; acts like full top and adds next text to next line (Flushes the output buffer)
@@ -39,6 +39,39 @@ std::cout <<d << '\n';  //20
 std::cout <<q << '\n'; //30
     return 0;
  }
+
+                                    //variables
+ #include <iostream>
+ int main(){     
+   int a=10;
+   double d=9.12;
+   char grade = 'A';
+   char initial = '$';
+   bool isAdult = true;
+   std::string name = "John Doe";
+   std::string day="tuesday";
+   
+std::cout <<a << '\n';
+std::cout <<d << '\n';
+std::cout <<grade << '\n';
+std::cout <<initial << '\n';
+std::cout <<isAdult << '\n';
+std::cout <<name << '\n';
+std::cout << day << '\n';
+std::cout << "HEllo " << name << "My Age is " <<a << "And my CGPA is " << d << '\n';
+    return 0;
+ }
+
+
+ //int : 4 bytes whole number (1,2,3,55,44,999,1000,10,1,-20)
+ //double : 8 bytes decimal number (1.5,2.3,3.14,44.99,100.001,-20.55)
+ //char : 1 byte single character ('A','B','C','D','a','b','c','d','1','2','3','@','#','$') use ' ' single Quote to represent character
+ //bool : 1 byte true or false (true, false) (1,0)
+ //string : varies multiple characters ("Hello","John Doe","Tuesday","$1000") use " " double Quote to represent string
+ 
+ //float : 4 bytes decimal number (1.5,2.3,3.14,44.99,100.001,-20.55) less precise than double
+ //long : 8 bytes whole number (1,2,3,55,44,999,1000,10,1,-20) larger range than int
+
 
 
 
