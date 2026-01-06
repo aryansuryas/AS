aa52501dc2dc68e7ada01e5085883b69518c2e11
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-01-06 23:29:47 +0530

    Update C++_COMPLETE_NOTES.cpp

--- C++_COMPLETE_NOTES.cpp
+++ C++_COMPLETE_NOTES.cpp
@@ -4,6 +4,13 @@
 // EXTENSION (.cpp)
 // Download all the Extension (C++/C  runner , .coderunenr) and in Vs code website MinGW dowmload Compiler 
 //  https://youtu.be/z2jDamkbBF0?si=jBpAcgaw3asPojT5  (WATCH HOW TO RUN C/C++ IN SYSTEM  )
- 
+
+
+                                     //BASIC CODE
+#include <iostream> //INPUT - OUTPUT STREAM
+int main(){
+    std::cout << "Hello, World!"  ;  // standard Character Output
+    return 0;
+}
 
 
