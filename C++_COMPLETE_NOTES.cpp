5272af94a045e6a2b3d4658b8c52c9d5f8b70e2b
Aryan Surya S <aryansuryasgowda@gmail.com>
2026-01-06 21:37:50 +0530

    Update C++_COMPLETE_NOTES.cpp

--- C++_COMPLETE_NOTES.cpp
+++ C++_COMPLETE_NOTES.cpp
@@ -2,4 +2,9 @@
 // Fast , Video Editting , Middle Level , Used for creating video games
 // high Level = Human readable Low Level= Machine Readable   c++ Middle Level
 // EXTENSION (.cpp)
-// Download all the Extension (C++/C  runner , .coderunenr) and in Vs code website MinGW dowmload Compiler
+// Download all the Extension (C++/C  runner , .coderunenr) and in Vs code website MinGW dowmload Compiler 
+-> pacman -Syu 
+->y
+->y
+then
+->MSY2 MSYS 
